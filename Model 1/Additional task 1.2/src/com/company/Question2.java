2c04c692ba2e71ca0ba94b1bac6641fb4661f423
Laiserk <savosarenskyrin@gmail.com>
2021-03-12 23:30:05 +0500

    Question2 completed

--- Model 1/Additional task 1.2/src/com/company/Question2.java
+++ Model 1/Additional task 1.2/src/com/company/Question2.java
@@ -2,10 +2,16 @@
 
 package com.company;
 
+import java.util.Scanner;
+
 public class Question2
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
 
+        int a  = sc.nextInt();
+
+        System.out.println(a / 10);
     }
 }
