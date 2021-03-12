5eadd51c142deb7fa7fa7765a522307e13c3dde1
Laiserk <savosarenskyrin@gmail.com>
2021-03-12 23:37:18 +0500

    Question3 completed

--- Model 1/Additional task 1.2/src/com/company/Question3.java
+++ Model 1/Additional task 1.2/src/com/company/Question3.java
@@ -2,10 +2,22 @@
 
 package com.company;
 
+import java.util.Scanner;
+
 public class Question3
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
 
+        int i  = sc.nextInt();
+
+        int a = i / 100;
+        int b = i / 10 % 10;
+        int c = i % 10;
+
+
+        int sum = a + b + c;
+        System.out.println(sum);
     }
 }
