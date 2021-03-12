576854a49e1df6edf1112dc774b58cae0c64c636
Laiserk <savosarenskyrin@gmail.com>
2021-03-12 23:43:13 +0500

    Question4 completed

--- Model 1/Additional task 1.2/src/com/company/Question4.java
+++ Model 1/Additional task 1.2/src/com/company/Question4.java
@@ -2,10 +2,18 @@
 
 package com.company;
 
+import java.util.Scanner;
+
 public class Question4
 {
     public static void main(String[] args)
     {
+        Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt(), b = sc.nextInt();
+
+        int c =  a * a + b * b;
+
+        System.out.println(c);
     }
 }
