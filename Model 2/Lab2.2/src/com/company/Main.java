185842340acbe0fb677598a2391505109aa8c4e3
Laiserk <savoarenskyrim@gmail.com>
2021-04-02 20:16:19 +0500

    LOL

--- Model 2/Lab2.2/src/com/company/Main.java
+++ Model 2/Lab2.2/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        var v = new Vector2D(1.2374, 2.2334);
+        var v2 = new Vector2D();
+        var v3 = new Vector2D(v);
+
+        v.print();
+        v2.print();
+        v3.print();
     }
 }
+
+
